a2ee4a6fd99bf84dde661e94dca37a304f31e53f
Adella <adellakenn@gmail.com>
2019-06-07 18:32:03 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,13 @@
-
+=begin
+  Backer - ::new
+  takes a name on initialization, accessible through an attribute reader (FAILED - 1)
+  initializes with a @backed_projects attribute, an empty array (FAILED - 2)
+=end
 
 class Backer
-  
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
   
